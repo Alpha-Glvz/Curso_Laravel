957ced1ac24ff0c642a1c3258e328fa74f93058f
Angel Galvez <galvezalpha@gmail.com>
2025-08-31 21:57:57 -0600

    Tema Rutas - Finalizado

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/servicios', function () {
     return view('servicios');
 });
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/crud', function () {
+    $age = 33;
+    $data = ['name' => 'Angel', 'age' => $age];
+    return view('crud/index',$data);
+})->name('crud');
