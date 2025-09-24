31c1d0783ee6fd7d852821be3896ae526cf841ef
Angel Galvez <galvezalpha@gmail.com>
2025-09-24 16:05:03 -0600

    restableciendo...

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\PrimerControlador;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[PrimerControlador::class,'index']);
-Route::get('/otro/{post}',[PrimerControlador::class,'otro']);
+Route::get('/', function() {
+    return view('welcome');
+});
